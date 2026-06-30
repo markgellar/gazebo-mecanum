e9b438fc338975a504d24f3a1d042cc4184a4536
Mark William <mark.w.gellar@gmail.com>
2026-06-29 20:43:18 -0400

    Add Gazebo launch file and ToF sensor plugins and orientation

---
+++ src/mecanum_robot/launch/gazebo.launch.py
@@ -0,0 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('mecanum_robot')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'mecanum_robot.urdf.xacro')
+
+    robot_description = xacro.process_file(urdf_file).toxml()
+
+    return LaunchDescription([
+        # Point Gazebo to mesh files
+        SetEnvironmentVariable(
+            name='GAZEBO_MODEL_PATH',
+            value=os.path.join(pkg_path, '..')
+        ),
+
+        # Start Gazebo
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen',
+        ),
+
+        # Publish URDF
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+        ),
+
+        # Spawn the robot in Gazebo
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-topic', 'robot_description', '-entity', 'mecanum_robot'],
+            output='screen',
+        ),
+    ])
